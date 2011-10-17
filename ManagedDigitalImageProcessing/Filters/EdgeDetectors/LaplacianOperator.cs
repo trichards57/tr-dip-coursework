67266eee8a003270749ce11cc9683ba40a39527e
Trichards57@gmail.com <Trichards57@gmail.com@4eac9e35-dea7-466d-b279-c35efa3e8ef3>
2011-10-17 22:31:57 +0000

    Added self-identification to filters. Set up Gaussian Filter calculate size based on sigma. Add no-op filter for ease of batch processing. Added LaplacianOperator.cs Combined filters in to one CannyFilter.cs for ease of batch processing.

---
+++ ManagedDigitalImageProcessing/Filters/EdgeDetectors/LaplacianOperator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ManagedDigitalImageProcessing.PGM;
+
+namespace ManagedDigitalImageProcessing.Filters.EdgeDetectors
+{
+    class LaplacianOperator : FilterBase
+    {
+        public override PgmImage Filter(PgmImage input)
+        {
+            var output = new PgmImage();
+            output.Header = input.Header;
+            output.Data = new byte[input.Data.Length];
+
+            Func<int, int, int> calculateIndex = ((x, y) => CalculateIndex(x, y, input.Header.Width, input.Header.Height));
+
+            var template = new int[] { 0, -1, 0, -1, 4, -1, 0, -1, 0 };
+
+            output.Data = Convolve(template, input.Data, new System.Drawing.Size(3, 3), new System.Drawing.Size(input.Header.Width, input.Header.Height));
+
+            return output;
+        }
+
+        public override string ToString()
+        {
+            return "Laplace";
+        }
+    }
+}
---
+++ ManagedDigitalImageProcessing/Filters/NoOpFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManagedDigitalImageProcessing.Filters
+{
+    class NoOpFilter : FilterBase
+    {
+        public override PGM.PgmImage Filter(PGM.PgmImage input)
+        {
+            return input;
+        }
+
+        public override string ToString()
+        {
+            return "NoOp";
+        }
+    }
+}
