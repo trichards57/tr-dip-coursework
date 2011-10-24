f6b32e1743f04b7fda63a061f30d490a7d6d6c9b
Trichards57@gmail.com <Trichards57@gmail.com@4eac9e35-dea7-466d-b279-c35efa3e8ef3>
2011-10-24 21:04:20 +0000

    Merging changes from Uni with those from home. Fixed NonMaximumSuppression to check in the correct direction.

--- ManagedDigitalImageProcessing/Filters/EdgeDetectors/NonMaximumResult.cs
+++ ManagedDigitalImageProcessing/Filters/EdgeDetectors/NonMaximumResult.cs
@@ -9,7 +9,7 @@ namespace ManagedDigitalImageProcessing.Filters.EdgeDetectors
         public PgmHeader Header { get; set; }
         public byte[] XData { get; set; }
         public byte[] YData { get; set; }
-        public bool[] Peak { get; set; }
+        public byte[] Peak { get; set; }
 
         public PgmImage ToPgmImage()
         {
@@ -19,8 +19,8 @@ namespace ManagedDigitalImageProcessing.Filters.EdgeDetectors
             {
                 for (var j = 0; j < Header.Height; j++)
                 {
-                    if (Peak[i + j * Header.Width])
-                        output.Data[i + j * Header.Width] = (byte)Math.Sqrt(XData[i + j * Header.Width] * XData[i + j * Header.Width] + YData[i + j * Header.Width] * YData[i + j * Header.Width]);
+                    if (Peak[i + j * Header.Width] > 0)
+                        output.Data[i + j * Header.Width] = Peak[i + j * Header.Width];
                     else
                         output.Data[i + j * Header.Width] = 0;
                 }
@@ -31,20 +31,7 @@ namespace ManagedDigitalImageProcessing.Filters.EdgeDetectors
 
         public Bitmap ToBitmap()
         {
-            var output = new Bitmap(Header.Width, Header.Height);
-
-            for (var i = 0; i < Header.Width; i++)
-            {
-                for (var j = 0; j < Header.Height; j++)
-                {
-                    if (Peak[i + j * Header.Width])
-                        output.SetPixel(i, j, Color.White);
-                    else
-                        output.SetPixel(i, j, Color.Black);
-                }
-            }
-
-            return output;
+            return ToPgmImage().ToBitmap();
         }
     }
 }
