60d8b7c8e40674d1372cbb51dfc8253b985a3a67
Trichards57@gmail.com <Trichards57@gmail.com@4eac9e35-dea7-466d-b279-c35efa3e8ef3>
2011-10-27 09:19:25 +0000

    Continuing transfer to MVVM.

--- DIPUI/ViewModels/CannyEdgeDetector.cs
+++ DIPUI/ViewModels/CannyEdgeDetector.cs
@@ -7,8 +7,40 @@ namespace DIPUI.ViewModels
 {
     class CannyEdgeDetector : EdgeDetector
     {
-        public CannyEdgeDetector() : base("Canny")
+        public CannyEdgeDetector()
+            : base("Canny")
         {
+        }
+
+        private int _upperThreshold = 100;
+        private int _lowerThreshold = 10;
+
+        public int UpperThreshold
+        {
+            get { return _upperThreshold; }
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentOutOfRangeException("value", value, "Value must be greater than 0.");
+                _upperThreshold = value;
+                OnPropertyChanged("UpperThreshold");
+                if (UpperThreshold < LowerThreshold)
+                    LowerThreshold = UpperThreshold;
+            }
+        }
+
+        public int LowerThreshold
+        {
+            get { return _lowerThreshold; }
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentOutOfRangeException("value", value, "Value must be greater than 0.");
+                _lowerThreshold = value;
+                OnPropertyChanged("LowerThreshold");
+                if (UpperThreshold < LowerThreshold)
+                    UpperThreshold = LowerThreshold;
+            }
         }
     }
 }
