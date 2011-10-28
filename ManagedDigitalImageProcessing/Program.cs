fa4e5fe15f53709764f4caadecec06e35d922c04
Trichards57@gmail.com <Trichards57@gmail.com@4eac9e35-dea7-466d-b279-c35efa3e8ef3>
2011-10-28 10:19:49 +0000

    Implemented native and filter.

--- ManagedDigitalImageProcessing/Program.cs
+++ ManagedDigitalImageProcessing/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using ManagedDigitalImageProcessing.Filters;
+using ManagedDigitalImageProcessing.PGM;
 
 namespace ManagedDigitalImageProcessing
 {
@@ -17,9 +18,33 @@ namespace ManagedDigitalImageProcessing
         {
             var inFile = File.Open(@"..\..\..\Base Images\foetus.pgm", FileMode.Open, FileAccess.Read, FileShare.Read);
 
-            var data = PGM.PgmLoader.LoadImage(inFile);
+            var data = PgmLoader.LoadImage(inFile);
+            var filter = new BitwiseAndFilter(0xF0);
 
-            NativeFilter.BitwiseAndFilter(data.Data, data.Data.Length, 0xF0);
+
+
+            var length = data.Data.Length;
+            var paddedLength = length + (16 - (length % 16)); // Increased so that the data fits perfectly in to the SSE2 registers.
+            var bytes = new byte[paddedLength];
+            Array.Copy(data.Data, bytes, length);
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (var i = 0; i < 50; i++)
+                NativeFilter.BitwiseAndFilter(paddedLength, bytes, 0xF0);
+
+            stopwatch.Stop();
+
+            var newStopwatch = new Stopwatch();
+            newStopwatch.Start();
+            for (var i = 0; i < 50; i++)
+                filter.Filter(data);
+            newStopwatch.Stop();
+
+            Console.WriteLine("Managed Function : {0}", newStopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Unmanaged Function : {0}", stopwatch.ElapsedMilliseconds);
+            Console.ReadLine();
         }
     }
 }
--- NativeDigitalImageProcessing/NativeDigitalImageProcessing.cpp
+++ NativeDigitalImageProcessing/NativeDigitalImageProcessing.cpp
@@ -4,8 +4,19 @@
 #include "stdafx.h"
 #include "NativeDigitalImageProcessing.h"
 
-extern "C" NATIVEDIGITALIMAGEPROCESSING_API HRESULT BitwiseAndFilter(char* data, int dataLength, char mask)
+extern "C" NATIVEDIGITALIMAGEPROCESSING_API HRESULT BitwiseAndFilter(int dataLength, char data[], char mask)
 {
+	int i;
+
+	for (i = 0; i < dataLength-15;i+=16)
+	{
+		__m128i	reg = _mm_loadu_si128((__m128i*)(data+i));
+		__m128i msk = _mm_set1_epi8(mask);
+
+		__m128i result = _mm_and_si128(reg,msk);
+
+		_mm_storeu_si128((__m128i*)(data+i), result);
+	}
 
 	return 0;
 }
